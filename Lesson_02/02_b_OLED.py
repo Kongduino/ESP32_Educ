49ce837696f6e00be184eb2c1643d0e882f64b8a
Kongduino <Kongduino@protonmail.com>
2026-01-01 15:42:39 +0800

    Lesson 02: OLED contrast

---
+++ Lesson_02/02_b_OLED.py
@@ -0,0 +1,27 @@
+import neopixel, time
+from machine import Pin, ADC
+
+adc_pin = Pin(13, mode=Pin.IN)
+adc = ADC(adc_pin)
+adc.atten(ADC.ATTN_11DB)
+lastRead = 0
+interval = 100
+lastValue = int(adc.read() / 16)
+display.contrast(lastValue)
+# This code inherits 'display' from boot.py
+
+while True:
+    if time.ticks_ms() - lastRead > interval:
+        now = int(adc.read() / 16)
+        if lastValue != now:
+            display.contrast(now)
+            print(f"Contrast: {now}")
+            if now == 0:
+                display.poweroff()
+            else:
+                display.poweron()
+            lastValue = now
+            lastRead = time.ticks_ms()
+
+
+
