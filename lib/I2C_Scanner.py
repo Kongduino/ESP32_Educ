0f25d50b26c445ed76ee20604b9e9485e97a9079
Kongduino <Kongduino@protonmail.com>
2026-01-04 17:19:48 +0800

    Working on Lesson 04: A Class Act

--- lib/I2C_Scanner.py
+++ lib/I2C_Scanner.py
@@ -2,10 +2,13 @@ class I2C_Scanner():
     def __init__(self, i2c):
         self._hasOLED = False
         self._has6DOF = False
+        self._hasTOF = False
         self._myI2C = i2c
         self._devices = self._myI2C.scan()
         if 60 in self._devices:
             self._hasOLED = True
+        if 41 in self._devices:
+            self._hasTOF = True
         if 104 in self._devices:
             self._has6DOF = True
 
@@ -16,4 +19,9 @@ class I2C_Scanner():
     @property
     def has6DOF(self):
         return self._has6DOF
+
+    @property
+    def hasTOF(self):
+        return self._hasTOF
+
 
